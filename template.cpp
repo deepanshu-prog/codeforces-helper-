0b49c98c8e7faf15bb6aed98e5d38e50c10aaedb
Deepanshu <88493862+deepanshu-prog@users.noreply.github.com>
2023-11-25 00:46:10 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define fo(i,a,b) for(ll i=a ; i<b ; i++)
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+
+typedef vector<ll> vll;
+typedef vector<int>	vi;
+typedef pair<int, int> pii;
+
+const int mod = 1000000007;
+//=============================================================
+void solve(){
+
+}
+//=============================================================
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+//====================== CODE =================================
+//    solve();
+//=============================================================
+	return 0;
+}
